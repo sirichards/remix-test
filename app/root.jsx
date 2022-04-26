364fa80e0b6deb74bec46b861e597cbda8d13264
Simon Richards <s.richards@dewynters.com>
2022-04-26 17:10:59 +0100

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -75,7 +75,7 @@ export default function App() {
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${defaults.GTM_ID}');</script>`)
     }
-  })
+  }, [])
 
   return (
     <html lang="en">
